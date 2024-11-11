a2f0fb0f62af800ce62e359e9f9e80b2a9112168
Thanh Tran <thanhcnttmcpe@gmail.com>
2024-11-11 10:26:34 +0700

    delete comments

--- backend/src/main/java/com/app/WebConfig.java
+++ backend/src/main/java/com/app/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Cho phép tất cả các endpoint
-                .allowedOrigins("http://localhost:3000") // Địa chỉ frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các phương thức HTTP được phép
-                .allowedHeaders("*"); // Cho phép tất cả các header
+        registry.addMapping("/**") 
+                .allowedOrigins("http://localhost:3000") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                .allowedHeaders("*"); 
     }
 }
