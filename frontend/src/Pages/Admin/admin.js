44014cf71850633342c82cd204f7c979ecd2044d
Thanh Tran <thanhcnttmcpe@gmail.com>
2024-11-15 23:43:39 +0700

    fixing admin.js

--- frontend/src/Pages/Admin/admin.js
+++ frontend/src/Pages/Admin/admin.js
@@ -1,9 +1,9 @@
 import React from "react";
-//import Button from "../../Components/Button";
 import { Link } from "react-router-dom";
 const tourPic = require("../../images/tourList.png");
 const addTourPic = require("../../images/addTour.png");
 const bookPic = require("../../images/book.png");
+const account_management = require("../../images/account_management.png");
 
 const Admin = () => {
   return (
@@ -11,90 +11,97 @@ const Admin = () => {
       style={{
         background: `linear-gradient(to right, #D2DAFF ,#EFEFEF, #B1B2FF)`,
         height: "100vh",
-        maxWidth: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <div className="row" style={{ maxWidth: "100%" }}>
-        <div className="col"></div>
-        <div className="col">
-          <div style={Styles.divStyle}>
-            <div className="mb-3" style={{ marginRight: "230px" }}>
-              <img src={tourPic}></img>
-              <Link
-                className="btn btn-info"
-                to={`/tourTable`}
-                style={Styles.buttonStyle}
-              >
-                Tour List
-              </Link>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </div>
-
-            <div className="mb-3" style={{ marginRight: "230px" }}>
-              <img src={addTourPic}></img>
-              <Link
-                className="btn btn-info"
-                to={`/addTour`}
-                style={Styles.buttonStyle}
-              >
-                Add Tour
-              </Link>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </div>
-
-            <div className="mb-3" style={{ marginRight: "230px" }}>
-              <img src={bookPic}></img>
-              <Link
-                className="btn btn-info"
-                to={`/bookingDetails`}
-                style={Styles.buttonStyle}
-              >
-                Bookings
-              </Link>
-              &nbsp;&nbsp;&nbsp;
-            </div>
-          </div>
+      <div style={Styles.container}>
+        <div style={Styles.cardContainer}>
+          <Card
+            image={account_management}
+            title="Accounts"
+            link="/manageAccounts"
+          />
+          <Card
+            image={tourPic}
+            title="Tour List"
+            link="/tourTable"
+          />
+          <Card
+            image={addTourPic}
+            title="Add Tour"
+            link="/addTour"
+          />
+          <Card
+            image={bookPic}
+            title="Bookings"
+            link="/bookingDetails"
+          />
         </div>
       </div>
     </div>
   );
 };
 
-const Styles = {
-  divStyle: {
-    //backgroundColor:"#F7ECDE",
-    //border:"solid",
-    borderStyle: "thin",
-    width: "40%",
-    display: "flex",
+const Card = ({ image, title, link }) => {
+  const [isHovered, setIsHovered] = React.useState(false);
 
-    margin: "auto",
-    marginTop: "30vh",
-    borderRadius: 30,
-    padding: "20px",
-    justifyContent: "center",
-    alignItems: "center",
-    //boxShadow: "3px 3px 10px 2px #C00000",
-    // backgroundColor:"#F0EBE3"
-  },
-  loginText: {
+  return (
+    <div style={Styles.card}>
+      <img src={image} style={Styles.imageStyle} alt={title} />
+      <Link
+        to={link}
+        style={{
+          ...Styles.buttonStyle,
+          backgroundColor: isHovered ? "#892318" : Styles.buttonStyle.backgroundColor,
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};
+
+const Styles = {
+  container: {
     textAlign: "center",
-    color: "#C00000",
-    fontStyle: "arial",
-    marginTop: 10,
+    padding: "20px",
+  },
+  title: {
+    fontSize: "2.5rem",
+    color: "#333",
+    marginBottom: "20px",
+  },
+  cardContainer: {
+    display: "flex",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+  },
+  card: {
+    width: "200px",
+    margin: "50px",
+    padding: "20px",
+    textAlign: "center",
+    transition: "transform 0.2s",
+  },
+  imageStyle: {
+    width: '150px',
+    height: '150px',
+    marginBottom: '10px',
   },
   buttonStyle: {
-    marginTop: 10,
-    position: "relative",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "50vh",
-    width: "10vw",
-    height: 40,
-    backgroundColor: "#D83A56",
+    display: "inline-block",
+    backgroundColor: "#e02c18",
     color: "white",
-    borderRadius: 5,
+    borderRadius: "5px",
     border: "none",
+    padding: "10px 20px",
+    textDecoration: "none",
+    fontWeight: "bold",
+    transition: "background-color 0.3s, transform 0.3s",
   },
 };
 
